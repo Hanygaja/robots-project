a3bca689f2980f0053b5512e661e503f055b2fca
Hanygaja <53864579+Hanygaja@users.noreply.github.com>
2019-10-31 20:39:50 +0300

    Updates

---
+++ precache-manifest.91b604b970e963c59a9126cfe900ae1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6a3e560f0413d3146cdd86a0982f1b",
+    "url": "/robots-project/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robots-project/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6e867908f435f1914a86",
+    "url": "/robots-project/static/css/main.7e73969b.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robots-project/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "6e867908f435f1914a86",
+    "url": "/robots-project/static/js/main.5f97c939.chunk.js"
+  },
+  {
+    "revision": "576714ede9648d4dcfa1",
+    "url": "/robots-project/static/js/runtime-main.e6473b0a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots-project/static/media/SEGA.12a3db8b.woff"
+  }
+]);
